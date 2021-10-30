1b86bbffc776cdd54a3ff642b5e1eb1a35165c24
Shashwat Garg <shashwatg2002@gmail.com>
2021-10-30 21:52:05 +0530

    Final, corrected some bugs and un-hardcoded value of M

--- Assignment 3/assignment_Bayesian_Estimation/code/Q1.m
+++ Assignment 3/assignment_Bayesian_Estimation/code/Q1.m
@@ -4,20 +4,13 @@ std_dev = 4;
 mean_val= 10;
 v = {5,10,20,40,60,80,100,500,1000,10000};
 n = size(v,2);
-ML_errors = zeros(n,100,'double');
-PM_errors_1 = zeros(n,100,'double');
-PM_errors_2 = zeros(n,100,'double');
-position_1 = zeros(n, 1, 'double');
-position_2 = zeros(n, 1, 'double');
-position_3 = zeros(n, 1, 'double');
-for i = 1:n
-    position_1(i) = 6*i;
-    position_2(i) = 6*i+2;
-    position_3(i) = 6*i+4;
-end
+samples = 200;
+ML_errors = zeros(n,samples,'double');
+PM_errors_1 = zeros(n,samples,'double');
+PM_errors_2 = zeros(n,samples,'double');
 
 for i = 1:n
-    [x,y,z] = error_arr(v{i}, mean_val, std_dev);
+    [x,y,z] = error_arr(v{i}, mean_val, std_dev, samples);
     ML_errors(i,:) = x;
     PM_errors_1(i,:) = y;
     PM_errors_2(i,:) = z;
@@ -27,7 +20,14 @@ v_str = cell(1,n);
 for i = 1:n
     v_str{i} = num2str(v{i});
 end
-
+position_1 = zeros(n, 1, 'double');
+position_2 = zeros(n, 1, 'double');
+position_3 = zeros(n, 1, 'double');
+for i = 1:n
+    position_1(i) = 6*i;
+    position_2(i) = 6*i+2;
+    position_3(i) = 6*i+4;
+end
 f = figure();
 b1= boxplot(ML_errors.','positions', position_1,'labels', v_str, 'boxstyle', 'filled', 'color', 'r');
 title('Error in different Estimates versus sample size')
@@ -35,21 +35,21 @@ xlabel('Sample Size')
 ylabel('Error in ML Estimate')
 
 hold on
+b3= boxplot(PM_errors_2.','positions', position_3,'labels', v_str, 'boxstyle', 'filled', 'color', 'b');
+hold on
 
 b2= boxplot(PM_errors_1.','positions', position_2,'labels', v_str, 'boxstyle', 'filled', 'color', 'g');
 
-hold on
 
-b3= boxplot(PM_errors_2.','positions', position_3,'labels', v_str, 'boxstyle', 'filled', 'color', 'b');
 hLegend = legend([b1(1), b2(1), b3(1)], {'MLE','Bayesian with Gaussian Prior', 'Bayesian with Uniform Prior'});
 
 saveas(f, "Q1.png")
 
-function [eaML, eaPM_1, eaPM_2] = error_arr (N, mean_val, std_dev)
-    eaML = zeros(100,1,'double');
-    eaPM_1 = zeros(100,1,'double');
-    eaPM_2 = zeros(100,1,'double');
-    for i = 1:100
+function [eaML, eaPM_1, eaPM_2] = error_arr (N, mean_val, std_dev, samples)
+    eaML = zeros(samples,1,'double');
+    eaPM_1 = zeros(samples,1,'double');
+    eaPM_2 = zeros(samples,1,'double');
+    for i = 1:samples
         d = dataset(N, mean_val, std_dev);
         eaML(i) = abs(lambda_ML(d) - mean_val)/mean_val;
         eaPM_1(i) = abs(lambda_PosteriorMean_1(d,10.5, 1, std_dev) - mean_val)/mean_val;
@@ -63,7 +63,7 @@ end
 function lpm = lambda_PosteriorMean_1(dat, prior_mean, prior_stddev, std_dev)
     n = size(dat,1);
     sample_mean = sum(dat)/n;
-    lpm = (sample_mean*prior_stddev*prior_stddev+prior_mean*std_dev*std_dev)/(std_dev*std_dev+prior_stddev*prior_stddev);
+    lpm = (sample_mean*prior_stddev*prior_stddev+prior_mean*std_dev*std_dev/n)/(std_dev*std_dev/n+prior_stddev*prior_stddev);
 end
 
 function lpm = lambda_PosteriorMean_2(dat, left, right)
--- Assignment 3/assignment_Bayesian_Estimation/code/Q2.m
+++ Assignment 3/assignment_Bayesian_Estimation/code/Q2.m
@@ -3,12 +3,19 @@ rng(1);
 
 v = {5,10,20,40,60,80,100,500,1000,10000};
 n = size(v,2);
-ML_errors = zeros(n,100,'double');
-PM_errors = zeros(n,100,'double');
+samples = 200;
+
+ML_errors = zeros(n,samples,'double');
+PM_errors = zeros(n,samples,'double');
 for i = 1:n
-    [x,y] = error_arr(v{i});
+    [x,y] = error_arr(v{i}, samples);
     ML_errors(i,:) = x;
     PM_errors(i,:) = y;
+end
+
+v_str = cell(1,n);
+for i = 1:n
+    v_str{i} = num2str(v{i});
 end
 position_1 = zeros(n, 1, 'double');
 position_2 = zeros(n, 1, 'double');
@@ -16,27 +23,22 @@ for i = 1:n
     position_1(i) = 4*i;
     position_2(i) = 4*i+2;
 end
-v_str = cell(1,n);
-for i = 1:n
-    v_str{i} = num2str(v{i});
-end
-
 f = figure();
-b1 = boxplot(ML_errors.','positions', position_1,'labels', v_str, 'boxstyle', 'filled', 'color', 'r')
+b1 = boxplot(ML_errors.','positions', position_1,'labels', v_str, 'boxstyle', 'filled', 'color', 'r');
 
 title('Error comparison of ML Estimate and Bayesian Estimate versus sample size')
 xlabel('Sample Size')
 ylabel('Error')
 hold on
-b2 = boxplot(PM_errors.','positions', position_2,'labels', v_str, 'boxstyle', 'filled', 'color', 'b')
+b2 = boxplot(PM_errors.','positions', position_2,'labels', v_str, 'boxstyle', 'filled', 'color', 'b');
 hLegend = legend([b1(1), b2(1)], {'MLE','Bayesian'});
 saveas(f, "Q2.png")
 
 
-function [eaML, eaPM] = error_arr (N)
-    eaML = zeros(100,1,'double');
-    eaPM = zeros(100,1,'double');
-    for i = 1:100
+function [eaML, eaPM] = error_arr (N,samples)
+    eaML = zeros(samples,1,'double');
+    eaPM = zeros(samples,1,'double');
+    for i = 1:samples
         d = dataset(N);
         eaML(i) = 0.2*abs(lambda_ML(d) - 5);
         eaPM(i) = 0.2*abs(lambda_PosteriorMean(d,5.5,1) - 5);
